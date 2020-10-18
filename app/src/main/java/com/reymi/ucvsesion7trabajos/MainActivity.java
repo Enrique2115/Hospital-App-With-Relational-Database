8849c85bf32908048e716084f5ab38c078e3fc27
Luis2115 <lmorochofebres@gmail.com>
2020-10-18 08:37:57 -0500

    Programando el inicio de la actividad mediante el boton

--- app/src/main/java/com/reymi/ucvsesion7trabajos/MainActivity.java
+++ app/src/main/java/com/reymi/ucvsesion7trabajos/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.reymi.ucvsesion7trabajos.sqlite.ControladorDAO;
+import com.reymi.ucvsesion7trabajos.ui.ListaCitas;
+import com.reymi.ucvsesion7trabajos.ui.RegAgenda;
 import com.reymi.ucvsesion7trabajos.ui.RegMedico;
 import com.reymi.ucvsesion7trabajos.ui.RegPaciente;
 
@@ -36,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegPaciente.class));
+            }
+        });
+
+        btnAgendarCita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegAgenda.class));
+            }
+        });
+
+        btnListarCitas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ListaCitas.class));
             }
         });
     }
