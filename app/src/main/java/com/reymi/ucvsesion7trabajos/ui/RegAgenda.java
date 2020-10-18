25508eb275d620506aab9fee720314e3632dfa59
Luis2115 <lmorochofebres@gmail.com>
2020-10-18 00:43:43 -0500

    Clase de la nueva actividad Agendar Cita

---
+++ app/src/main/java/com/reymi/ucvsesion7trabajos/ui/RegAgenda.java
@@ -0,0 +1,16 @@
+package com.reymi.ucvsesion7trabajos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.reymi.ucvsesion7trabajos.R;
+
+public class RegAgenda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_agenda);
+    }
+}
