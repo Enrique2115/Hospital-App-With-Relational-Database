ff83802c8dd5e674caf591683bbbfb293ef78501
Luis2115 <lmorochofebres@gmail.com>
2020-10-18 08:45:22 -0500

    Creacion del activity Listar Citas

---
+++ app/src/main/java/com/reymi/ucvsesion7trabajos/ui/ListaCitas.java
@@ -0,0 +1,17 @@
+package com.reymi.ucvsesion7trabajos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.reymi.ucvsesion7trabajos.R;
+
+public class ListaCitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_citas);
+
+    }
+}
